873a1ad56406279dbdace4636e6d16d578949cb6
hahan <hvha1@cmcglobal.vn>
2025-03-13 17:15:51 +0700

    (+) create table & index value column

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -9,5 +9,16 @@ export const initializeDatabase = async () => {
   });
 
   await connection.query(`CREATE DATABASE IF NOT EXISTS fraud_codes`);
+  await connection.query(`
+    CREATE DATABASE IF NOT EXISTS fraud_codes;
+    USE fraud_codes;
+    CREATE TABLE IF NOT EXISTS fraud_codes (
+      id CHAR(36) PRIMARY KEY,
+      value VARCHAR(255) NOT NULL,
+      createdDate VARCHAR(255) NOT NULL,
+      usedDate VARCHAR(255) DEFAULT NULL,
+      INDEX (value)
+    );
+  `);
   await connection.end();
 };
